3285e3694c144060f54df737f9bfdd5f88a385b3
Daniel Axtens <dja@axtens.net>
2018-02-22 01:17:11 +1100

    tools/scripts: parallel_parsearchive - load archives in parallel
    If you have multiple archives, you quickly tire of typing stuff like
    python3 manage.py parsearchive --list-id=patchwork.ozlabs.org foo-1 &
    python3 manage.py parsearchive --list-id=patchwork.ozlabs.org foo-2 &
    python3 manage.py parsearchive --list-id=patchwork.ozlabs.org foo-3 &
    python3 manage.py parsearchive --list-id=patchwork.ozlabs.org foo-4 &
    and having to copy and paste it - or retype it! - each time you reset
    the database.
    
    Instead, this patch allows you to do
    tools/scripts/parallel_parsearchive.sh --list-id=patchwork.ozlabs.org -- foo-*
    
    Much easier, especially when you are doing it a dozen times.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>

---
+++ tools/scripts/parallel_parsearchive.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Patchwork - automated patch tracking system
+# Copyright (C) 2018 Daniel Axtens <dja@axtens.net>
+#
+# This file is part of the Patchwork package.
+#
+# Patchwork is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Patchwork is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+set -euo pipefail
+
+usage() {
+    cat <<EOF
+parallel_parsearchive.sh - load archives in parallel
+Usage:
+  parallel_parsearchive.sh [parsearchive options] -- <archives>
+  The -- is mandatory.
+  As many processes as there are archives will be spun up.
+
+Example:
+EOF
+    exit 1
+}
+
+if [ $# -eq 0 ] || [[ $1 == "-h" ]]; then
+    usage;
+fi
+
+PARSEARCHIVE_OPTIONS=""
+while [[ $1 != "--" ]]; do
+    PARSEARCHIVE_OPTIONS="$PARSEARCHIVE_OPTIONS $1"
+    shift
+    if [ $# -eq 0 ]; then
+        usage;
+    fi
+done
+shift
+
+if [ $# -eq 0 ]; then
+    usage;
+fi
+
+
+for x in "$@"; do
+    echo "Starting $x"
+    python3 manage.py parsearchive $PARSEARCHIVE_OPTIONS "$x" &
+done
+echo "Processes started in the background."
